343637477a70d2230ad8d301cf2d3533c0775285
18behrens <8behrens@informatik.uni-hamburg.de>
2022-11-12 16:03:48 +0100

    add model weights + alter test.py to test with own images (test.png)

--- code/test.py
+++ code/test.py
@@ -5,32 +5,26 @@ os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"
 os.environ["CUDA_VISIBLE_DEVICES"]="0"
 import argparse
 import numpy as np 
-import math
 import torch
-import torch.nn as nn
-import torch.nn.functional as F
-import torch.optim as optim
-from torchvision import datasets, transforms
 from PIL import Image
-from ema import EMA
-from datasets import MnistDataset
-from transforms import RandomRotation
 from models.modelM3 import ModelM3
 from models.modelM5 import ModelM5
 from models.modelM7 import ModelM7
+from skimage.color import rgb2gray, rgba2rgb
 
 def run(p_seed=0, p_kernel_size=5, p_logdir="temp"):
 
     # enable GPU usage ------------------------------------------------------------#
     use_cuda = torch.cuda.is_available()
-    device = torch.device("cuda" if use_cuda else "cpu")
-    if use_cuda == False:
-        print("WARNING: CPU will be used for training.")
-        exit(0)
+    # device = torch.device("cuda" if use_cuda else "cpu")
+    device = torch.device("cpu")
+    # if use_cuda == False:
+    #     print("WARNING: CPU will be used for training.")
+    #     exit(0)
 
     # data loader -----------------------------------------------------------------#
-    test_dataset = MnistDataset(training=False, transform=None)
-    test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=100, shuffle=False)
+    # test_dataset = MnistDataset(training=False, transform=None)
+    # test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=100, shuffle=False)
 
     # model selection -------------------------------------------------------------#
     if(p_kernel_size == 3):
@@ -43,26 +37,43 @@ def run(p_seed=0, p_kernel_size=5, p_logdir="temp"):
     model1.load_state_dict(torch.load("../logs/%s/model%03d.pth"%(p_logdir,p_seed)))
 
     model1.eval()
-    test_loss = 0
-    correct = 0
-    wrong_images = []
-    with torch.no_grad():
-        for batch_idx, (data, target) in enumerate(test_loader):
-            data, target = data.to(device), target.to(device)
-            output = model1(data)
-            test_loss += F.nll_loss(output, target, reduction='sum').item()
-            pred = output.argmax(dim=1, keepdim=True)
-            correct += pred.eq(target.view_as(pred)).sum().item()
-            wrong_images.extend(np.nonzero(~pred.eq(target.view_as(pred)).cpu().numpy())[0]+(100*batch_idx))
 
-    np.savetxt("../logs/%s/wrong%03d.txt"%(p_logdir,p_seed), wrong_images, fmt="%d")
+    with torch.no_grad():
+        img = np.array(Image.open("../test.png"))
+        if img.shape[2] == 4:
+            img = rgba2rgb(img)
+        if img.shape[2] == 3:
+            img = rgb2gray(img)
+        img = 1 - img
+        data = torch.tensor(img, dtype=torch.float32)
+        data = data.to(device)
+        data = data.reshape((1, 1, 28, 28))
+        output = model1(data)
+        print("Wahrscheinlichkeit, für die verschiedenen Zahlen", output)
+        pred = output.argmax(dim=1, keepdim=True)
+        print("Vermutung, welche Zahl es ist", pred)
+
+    # test_loss = 0
+    # correct = 0
+    # wrong_images = []
+    # with torch.no_grad():
+    #     for batch_idx, (data, target) in enumerate(test_loader):
+    #         data, target = data.to(device), target.to(device)
+    #         output = model1(data)
+    #         # test_loss += F.nll_loss(output, target, reduction='sum').item()
+    #         pred = output.argmax(dim=1, keepdim=True)
+    #         print(pred)
+    #         correct += pred.eq(target.view_as(pred)).sum().item()
+    #         wrong_images.extend(np.nonzero(~pred.eq(target.view_as(pred)).cpu().numpy())[0]+(100*batch_idx))
+    #
+    # np.savetxt("../logs/%s/wrong%03d.txt"%(p_logdir,p_seed), wrong_images, fmt="%d")
     #print(len(wrong_images), wrong_images)
 
 if __name__ == "__main__":
     p = argparse.ArgumentParser()
     p.add_argument("--logdir", default="modelM5")
     p.add_argument("--seed", default=0, type=int)
-    p.add_argument("--trials", default=30, type=int)
+    p.add_argument("--trials", default=1, type=int)
     p.add_argument("--kernel_size", default=5, type=int)
     args = p.parse_args()
     for i in range(args.trials):
